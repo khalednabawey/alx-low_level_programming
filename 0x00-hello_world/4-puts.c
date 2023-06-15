cf5cf23f2ebdfa58f1c07fa04da8ee1b726a211d
khalednabawey <5alednabawi@gmail.com>
2023-06-15 11:49:42 -0700

    tt55

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,6 @@
-# !/ bin / bash
-puts("Programming is like building a multilingual puzzle");
-reurn 0;
+#include<stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
