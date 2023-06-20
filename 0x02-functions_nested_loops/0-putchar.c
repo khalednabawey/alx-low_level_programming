78ed6b932b90d0476009444f82f4f21273a7dbfc
khalednabawey <5alednabawi@gmail.com>
2023-06-20 05:16:22 -0700

    t0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include<main.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char s[] = "_putchar";
+
+	for (int i = 0; i <= 7; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
+
