c92ba9216949410cffdab0eccb4dcd2687624818
khalednabawey <5alednabawi@gmail.com>
2023-06-20 11:41:00 -0700

    tt2121

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,17 +9,21 @@ void print_times_table(int n)
 {
 	int i, j, r;
 
-	for (i = 0; i <= n; i++)
+	if (n <= 15 && n >= 0)
 	{
-		_putchar('0');
-		for (j = 1; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			r = i * j;
-			_putchar(',');
-			_putchar(' ');
-			(r / 10) ? _putchar('0' + r / 10) : _putchar(' ');
-			_putchar('0' + r % 10);
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+			{
+				r = i * j;
+				_putchar(',');
+				_putchar(' ');
+				(r > 99) ? _putchar('0' + r / 100) : _putchar(' ');
+				(r > 9) ? _putchar('0' + r / 10 % 10) : _putchar(' ');
+				_putchar('0' + r % 10);
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
