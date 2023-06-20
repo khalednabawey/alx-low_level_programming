7d1482237db00e6c6e2318ef4c3e3a274d5ed973
khalednabawey <5alednabawi@gmail.com>
2023-06-20 06:24:06 -0700

    t1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i = 'a';
+
+	for (; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
