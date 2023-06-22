89ff8a931101a756428f44613615fa5ba8888c72
khalednabawey <5alednabawi@gmail.com>
2023-06-22 05:49:21 -0700

    t0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,12 +5,13 @@
  * is a function to check whether the character is upper or not.
  *
  * @c: c is an integer parameter
- * Return: int
+ * Return: int 1 if c is upper 0 otherwise.
  */
 
 int _isupper(int c);
 {
 	if (c >= 65 && c <= 90)
 		return (1);
-	return (0);
+	else
+		return (0);
 }
