cfec09a68a543795df793bbce7af289f2b0ed056
khalednabawey <5alednabawi@gmail.com>
2023-06-22 08:21:21 -0700

    t1

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal ->
+ * is a function that prints a square, followed by a new line.
+ *
+ * @size: size and integer parameter.
+ *
+ * Return: void(no return).
+ */
+
+void print_square(int size);
+{
+	if (n > 0)
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
