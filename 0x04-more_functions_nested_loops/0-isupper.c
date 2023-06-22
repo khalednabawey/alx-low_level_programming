212653e3ba234d71f20f8e6bc91d722ae0f2fa05
khalednabawey <5alednabawi@gmail.com>
2023-06-22 05:37:24 -0700

    tt0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower ->
+ * is a function to check whether the character is upper
+ *
+ * @c: c is an integer parameter
+ * Return: int
+ */
+
+int _isupper(int c);
+{
+	if (c >= 97 && c <= 122)
+		return (0);
+	return (1);
+}
