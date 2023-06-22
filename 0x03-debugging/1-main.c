3c01c3c079ba063c435b8336c02875d02f2a135f
khalednabawey <5alednabawi@gmail.com>
2023-06-22 03:54:18 -0700

    tt11

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,9 +7,8 @@
 
 int main(void)
 {
-/*
- * int i;
-*/
+	int i;
+
 	printf("Infinite loop incoming :(\n");
 /*
  *	i = 0;
@@ -20,8 +19,6 @@ int main(void)
  *		putchar(i);
  *	}
  */
-
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
