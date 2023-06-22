355b86d14f4e072be754f628f4808bd98a719930
khalednabawey <5alednabawi@gmail.com>
2023-06-22 05:57:48 -0700

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,10 +8,9 @@
  * Return: int 1 if c is upper 0 otherwise.
  */
 
-int _isupper(int c);
+int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 		return (1);
-	else
-		return (0);
+	return (0);
 }
