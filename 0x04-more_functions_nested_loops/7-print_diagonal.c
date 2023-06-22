3702e762c26895be6bb6fbe36dab9e48d02ef39e
khalednabawey <5alednabawi@gmail.com>
2023-06-22 08:06:27 -0700

    tt77

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,18 +10,21 @@
 
 void print_diagonal(int n)
 {
-	int i, j;
-
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		j = 0;
-		while (j < i)
-		{
-			_putchar(' ');
-			j++;
-		}
-		_putchar('\\');
-		_putchar('\n');
-	}
-}
+		int i, j;
 
+		for (i = 0; i < n; i++)
+		{
+			j = 0;
+			while (j < i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
