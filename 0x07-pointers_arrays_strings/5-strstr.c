36d2118e28b438b4c4de7b351d319787e70d6b8c
khalednabawey <5alednabawi@gmail.com>
2023-07-03 08:28:04 -0700

    t5555

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,23 +10,21 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	if (*needle == '\0')
-		return (haystack);
+	char *s, *p;
 
 	while (*haystack != '\0')
 	{
-		char *s = haystack;
-		char *p = needle;
-	}
-	while (*haystack != '\0' && *p != '\0' && *haystack == *p)
-	{
-		haystack++;
-		p++;
-	}
+		s = haystack;
+		p = needle;
 
-	if (*p == '\0')
-		return (s);
-	haystack = s + 1;
+		while (*haystack != '\0' && *p != '\0' && *haystack == *p)
+		{
+			haystack++;
+			p++;
+		}
+		if (*p == '\0')
+			return (s);
+		haystack = s + 1;
 	}
 	return (0);
 }
