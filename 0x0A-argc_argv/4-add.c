ff1794201191461e918b76c1d05193ac1670bf68
khalednabawey <5alednabawi@gmail.com>
2023-07-07 07:13:19 -0700

    t4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -> Entry point.
+ *
+ * @argc: This is the argument count.
+ * @argv: This is the argument vector.
+ *
+ * Return: This return to 0.
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, i;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			int j;
+			char *arg;
+
+			arg = argv[i];
+			for (j = 0; arg[j] != '\0'; j++)
+			{
+				if (arg[j] < 48 || arg[j] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
