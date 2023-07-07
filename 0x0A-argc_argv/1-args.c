ba322ccbe44313af3f3b4123555f9efeb8e095d2
khalednabawey <5alednabawi@gmail.com>
2023-07-07 06:19:39 -0700

    t11

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -> Write a program that prints the number of argumenst,
+ * followed by a new line.
+ *
+ * @argc: This is the argument count.
+ * @argv: This is the argument vector.
+ *
+ * Return: This return to 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
