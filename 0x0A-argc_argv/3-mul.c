1439e5442bfb48f37567dc94453ca06fd547b7a5
khalednabawey <5alednabawi@gmail.com>
2023-07-07 06:43:34 -0700

    t33

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -> Entry point.
+ *
+ * @argc: This is the argument count.
+ * @argv: This is the argument vector.
+ *
+ * Return: This return to 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", (int)argv[1] * (int)argv[2]);
+		return (0);
+	}
+}
