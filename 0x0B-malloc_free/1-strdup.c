877fb4ade1d5aa72adcf6126fb19e55a997da2ac
khalednabawey <5alednabawi@gmail.com>
2023-07-11 01:52:00 -0700

    t101

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,27 +11,26 @@
  */
 char *_strdup(char *str)
 {
-    int i, size = 0;
+	int i, size = 0;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    while (str[size] != '\0')
-        size++;
+	while (str[size] != '\0')
+		size++;
 
-    char *arr = malloc((size + 1) * sizeof(char));
+	char *arr = malloc((size + 1) * sizeof(char));
 
-    if (arr == NULL)
-    {
-        fprintf(stderr, "Memory allocation failed\n");
-        return (NULL);
-    }
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        *(arr + i) = *(str + i);
-    }
-    arr[size] = '\0'; // Add the null terminator at the end
+	for (i = 0; i < size; i++)
+	{
+		*(arr + i) = *(str + i);
+	}
+	arr[size] = '\0';
 
-    return (arr);
+	return (arr);
 }
