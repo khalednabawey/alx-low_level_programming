cef66d3fe5f3dabed052e17f31181fb801236ba1
khalednabawey <5alednabawi@gmail.com>
2023-07-13 06:01:53 -0700

    t1212

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,13 +12,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *r;
 
-	if (nmemb == 0)
-		return (NULL);
-	else if (size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	r = malloc(nmemb * size);
 
 	if (r == NULL)
 		return (NULL);
+
+	return (r);
 }
+
