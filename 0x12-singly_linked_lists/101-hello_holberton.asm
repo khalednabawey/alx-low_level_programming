6b93302610bf0c89fc7bcd431b98313367632ba4
khalednabawey <5alednabawi@gmail.com>
2023-07-27 07:30:14 -0700

    asm101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,18 @@
 section .data
-    hello db "Hello, Holberton", 0xA, 0
+    hello: db "Hello, Holberton", 10, 0
 
 section .text
-    global _start
+    global main
+    extern printf
 
-_start:
-    ; Call printf function
-    mov rdi, hello    ; Load the address of the hello string into rdi (1st argument)
-    mov rax, 0        ; Use rax as syscall number 0 (syscall for printf)
-    call printf      ; Call the printf function
+main:
+    push rbp
+    mov rbp, rsp
 
-    ; Exit the program
-    mov rax, 60      ; Use rax as syscall number 60 (syscall for exit)
-    xor rdi, rdi     ; Return 0 status
-    syscall         ; Perform syscall to exit the program
+    lea rdi, [hello]
+    xor eax, eax
+    call printf
 
+    mov eax, 0
+    leave
+    ret
