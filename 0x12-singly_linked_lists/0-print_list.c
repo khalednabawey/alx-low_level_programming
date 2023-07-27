455898f00263cf27a14fc3cc645a06bc3599fac0
khalednabawey <5alednabawi@gmail.com>
2023-07-27 06:53:42 -0700

    tt00

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,15 +15,15 @@
 size_t print_list(const list_t *h)
 {
 	size_t size = 0;
-	list_t *ptr;
 
-	ptr = h;
-	while (ptr != NULL)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		size++;
-		ptr = ptr->next;
+		h = h->next;
 	}
 	return (size);
 }
