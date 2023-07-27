7dc5fa10613212afbbd96125c1c640d748398ef0
khalednabawey <5alednabawi@gmail.com>
2023-07-27 07:14:59 -0700

    task22

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,19 +14,26 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	if (str == NULL)
-		return (NULL);
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
+	char *str_copy;
+	int str_len = 0;
 
+	while (str[str_len])
+		str_len++;
+
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
+	str_copy = strdup(str);
+	if (str_copy == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
+
+	new_node->str = str_copy;
+	new_node->len = str_len;
 	new_node->next = *head;
 	*head = new_node;
 
