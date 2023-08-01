4ab2f492575d47ef328adb1352e27cbf481461ee
khalednabawey <5alednabawi@gmail.com>
2023-08-01 02:51:52 -0700

    t1022

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a linked list safely.
+ * free_listint_safe - frees a linked list.
  *
  * @h: pointer to the first node in the linked list.
  *
