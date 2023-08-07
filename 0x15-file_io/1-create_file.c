d06cddac4458632aedef682b8cefa7d80efee7d5
khalednabawey <5alednabawi@gmail.com>
2023-08-07 06:24:37 -0700

    t1112

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,9 +26,11 @@ int create_file(const char *filename, char *text_content)
 
 
 	while (text_content[i] != '\0')
+	{
 		i++;
+	}
 
-	file = open(filename, O_WRONLY | O_APPEND);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file == -1)
 		return (-1);
